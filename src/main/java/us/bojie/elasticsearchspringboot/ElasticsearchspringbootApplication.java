70bd8c88793a41bc8ae6f9987b4c48eaaa971c0c
Bojie Jiang <jiangbojie@gmail.com>
2017-10-02 00:05:15 -0700

    Adding book/novel interface

--- src/main/java/us/bojie/elasticsearchspringboot/ElasticsearchspringbootApplication.java
+++ src/main/java/us/bojie/elasticsearchspringboot/ElasticsearchspringbootApplication.java
@@ -1,45 +1,80 @@
 package us.bojie.elasticsearchspringboot;
 
 import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.Date;
 
 @SpringBootApplication
 @RestController
 public class ElasticsearchspringbootApplication {
 
-	@Autowired
-	private TransportClient mClient;
+    @Autowired
+    private TransportClient mClient;
 
-	public static void main(String[] args) {
-		SpringApplication.run(ElasticsearchspringbootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchspringbootApplication.class, args);
+    }
 
-	@GetMapping("/")
-	public String index() {
-		return "index";
-	}
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
 
-	@GetMapping("/get/book/novel")
-	public ResponseEntity get(@RequestParam(name = "id", defaultValue = "") String id) {
-		if (id.isEmpty()) {
-			return new ResponseEntity(HttpStatus.NOT_FOUND);
-		}
+    @GetMapping("/get/book/novel")
+    public ResponseEntity get(@RequestParam(name = "id", defaultValue = "") String id) {
+        if (id.isEmpty()) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
 
-		GetResponse result = mClient.prepareGet("book", "novel", id)
-				.get();
+        GetResponse result = mClient.prepareGet("book", "novel", id)
+                .get();
 
-		if (!result.isExists()) {
-			return new ResponseEntity(HttpStatus.NOT_FOUND);
-		}
+        if (!result.isExists()) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
 
-		return new ResponseEntity(result.getSource(), HttpStatus.OK);
-	}
+        return new ResponseEntity(result.getSource(), HttpStatus.OK);
+    }
+
+    @PostMapping("add/book/novel")
+    public ResponseEntity add(
+            @RequestParam(name = "title") String title,
+            @RequestParam(name = "author") String author,
+            @RequestParam(name = "word_count") int wordCount,
+            @RequestParam(name = "publish_date")
+            @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+                    Date publishDate) {
+        try {
+            XContentBuilder content = XContentFactory.jsonBuilder()
+                    .startObject()
+                    .field("title", title)
+                    .field("author", author)
+                    .field("word_count", wordCount)
+                    .field("publish_date", publishDate.getTime())
+                    .endObject();
+            IndexResponse result = mClient.prepareIndex("book", "novel")
+                    .setSource(content)
+                    .get();
+            return new ResponseEntity(result.getId(), HttpStatus.OK);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
 }
