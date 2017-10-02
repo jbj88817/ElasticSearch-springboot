92334b84073c5b68a394930e7dba039490b94fbe
Bojie Jiang <jiangbojie@gmail.com>
2017-10-01 23:50:39 -0700

    Get book/novel interface

--- src/main/java/us/bojie/elasticsearchspringboot/ElasticsearchspringbootApplication.java
+++ src/main/java/us/bojie/elasticsearchspringboot/ElasticsearchspringbootApplication.java
@@ -1,13 +1,22 @@
 package us.bojie.elasticsearchspringboot;
 
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.client.transport.TransportClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class ElasticsearchspringbootApplication {
+
+	@Autowired
+	private TransportClient mClient;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ElasticsearchspringbootApplication.class, args);
@@ -16,5 +25,21 @@ public class ElasticsearchspringbootApplication {
 	@GetMapping("/")
 	public String index() {
 		return "index";
+	}
+
+	@GetMapping("/get/book/novel")
+	public ResponseEntity get(@RequestParam(name = "id", defaultValue = "") String id) {
+		if (id.isEmpty()) {
+			return new ResponseEntity(HttpStatus.NOT_FOUND);
+		}
+
+		GetResponse result = mClient.prepareGet("book", "novel", id)
+				.get();
+
+		if (!result.isExists()) {
+			return new ResponseEntity(HttpStatus.NOT_FOUND);
+		}
+
+		return new ResponseEntity(result.getSource(), HttpStatus.OK);
 	}
 }
